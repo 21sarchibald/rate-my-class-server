93ab39aba1c27019867828644d35929c08971bbf
Sophie Archibald <arc21004@byui.edu>
2026-07-07 21:41:56 -0600

    Initialized reviews and courses db

--- src/database/init-db.js
+++ src/database/init-db.js
@@ -18,6 +18,8 @@ const seedUsers = async (db) => {
 
     const hashedPassword2 = await argon2.hash("Test456!");
 
+    const hashedPassword3 = await argon2.hash("Test789!");
+
     const users = [
         {
             name: "Alice Student",
@@ -36,6 +38,16 @@ const seedUsers = async (db) => {
             password: hashedPassword2,
             major: "Software Engineering",
             userType: "Admin",
+            createdAt: Date.now(),
+            updatedAt: Date.now()
+        },
+        {
+            name: "Sam Student",
+            email: "sam.student@test.com",
+            username: "sstudent",
+            password: hashedPassword3,
+            major: "Web Design and Development",
+            userType: "Student",
             createdAt: Date.now(),
             updatedAt: Date.now()
         }
@@ -59,6 +71,103 @@ const seedUsers = async (db) => {
     }
 }
 
+const seedClasses = async (db) => {
+
+    const classes = [
+        {
+            courseCode: "CSE210",
+            courseName: "Programming with Classes",
+            createdAt: Date.now(),
+            updatedAt: Date.now()
+        },
+        {
+            courseCode: "WDD130",
+            courseName: "Web Fundamentals",
+            createdAt: Date.now(),
+            updatedAt: Date.now()
+        }
+    ]
+
+    try {
+        await db.collection("classes").drop();
+        console.log("classes dropped");
+
+        await db.createCollection("classes");
+        console.log("classess collection created")
+
+        const result = await db.collection("classes").insertMany(
+            classes
+        );
+
+        console.log(result.insertedIds);
+    }
+    catch (error) {
+        console.log(error.message)
+    }
+}
+
+const seedReviews = async (db) => {
+
+    const user1 = await db.collection("users").findOne({ email: "alice.student@test.com" });
+    const user2 = await db.collection("users").findOne({ email: "sam.student@test.com" });
+
+    const course1 = await db.collection("classes").findOne({ courseCode: "CSE210" });
+    const course2 = await db.collection("classes").findOne({ courseCode: "WDD130" });
+
+    const reviews = [
+        {
+            userId: user1._id,
+            courseId: course1._id,
+            courseName: "Programming with Classes",
+            rating: 5,
+            difficulty: 4,
+            likelihoodToRecommend: 5,
+            professor: "Gary Godderidge",
+            semester: "Fall",
+            year: 2024,
+            type: "in-person",
+            isBlock: true,
+            description: "Loved this class! Fast-paced but really worth it. I felt like I came out of there with great knowledge and skills to prepare me for the next class.",
+            gradeReceived: "A",
+            createdAt: Date.now(),
+            updatedAt: Date.now()
+        },
+        {
+            userId: user2._id,
+            courseId: course2._id,
+            rating: 3,
+            difficulty: 2,
+            likelihoodToRecommend: 2,
+            professor: "James Christensen",
+            semester: "Winter",
+            year: 2026,
+            type: "online",
+            isBlock: false,
+            description: "Brother Christensen is a nice teacher, but the course material is hard to understand and takes a long time to read. Probably would have been better in-person.",
+            gradeReceived: "B-",
+            createdAt: Date.now(),
+            updatedAt: Date.now()
+        }
+    ]
+
+    try {
+        await db.collection("reviews").drop();
+        console.log("reviews dropped");
+
+        await db.createCollection("reviews");
+        console.log("reviews collection created")
+
+        const result = await db.collection("reviews").insertMany(
+            reviews
+        );
+
+        console.log(result.insertedIds);
+    }
+    catch (error) {
+        console.log(error.message)
+    }
+}
+
 const init = async () => {
     try {
         await client.connect();
@@ -70,8 +179,18 @@ const init = async () => {
         // initialize Users collection
         await seedUsers(db);
         console.log(`Collection 'users' initialized successfully`);
+
+        // initialize Classes collection
+        await seedClasses(db);
+        console.log(`Collection 'classes' initialized successfully`);
+
+        // initialize Reviews collection
+        await seedReviews(db);
+        console.log(`Collection 'reviews' initialized successfully`);
+
     } catch (error) {
         console.error(error.message);
+
     } finally {
         await client.close();
     }
